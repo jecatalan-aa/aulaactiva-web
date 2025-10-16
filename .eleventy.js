dec9056955d06b5834b7b9369dd13aba43669024
José Catalán <josecatalan@MacBook-Pro-de-Jose.local>
2025-10-16 11:11:19 -0300

    ajustes a index y base.njk antes de separar paginas 2

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     // Le decimos a Eleventy que copie estas carpetas al sitio final tal cual.
     eleventyConfig.addPassthroughCopy("assets/images");
+    eleventyConfig.addPassthroughCopy("images"); // <-- AÑADE ESTA LÍNEA
     eleventyConfig.addPassthroughCopy("css"); 
   
     return {
